82a32dd007993a2cbd68bacdf347d208a375f959
Super Developer <712020:cb96413f-3204-4598-bfed-7951c287918d@bots.bitbucket.org>
2023-06-10 22:19:08 +0800

    code cleanup

--- src/instrument/instrument.controller.ts
+++ src/instrument/instrument.controller.ts
@@ -1,37 +1,22 @@
-import {
-    Body,
-    Controller,
-    Get,
-    Logger,
-    Param,
-    Post,
-    Query,
-    Res,
-    UseGuards,
-  } from '@nestjs/common';
-  import { Observable, lastValueFrom } from 'rxjs';
-  import { Response } from 'express';
+import { Controller, Get, Logger, Param } from '@nestjs/common';
 import { InstruementService } from './instrument.service';
-  
-  @Controller('instrument')
-  export class InstrumentController {
-    private readonly logger = new Logger(InstrumentController.name);
-  
-    constructor(private readonly instrumentService: InstruementService) {}
-  
-    @Get('health-check')
-    healthCheck(): any {
-      return this.instrumentService.healthCheck();
-    }
-    @Get('/generate-session/:token')
-    generateSession(@Param('token') token: string): any {
-      return this.instrumentService.generateSession(token);
-    }
-    @Get('/load-instrument')
-    loadInstrument(): any {
-      return this.instrumentService.getInstruments();
-    }
-    
-     
+
+@Controller('instrument')
+export class InstrumentController {
+  private readonly logger = new Logger(InstrumentController.name);
+
+  constructor(private readonly instrumentService: InstruementService) {}
+
+  @Get('health-check')
+  healthCheck(): any {
+    return this.instrumentService.healthCheck();
   }
-  
+  @Get('/generate-session/:token')
+  generateSession(@Param('token') token: string): any {
+    return this.instrumentService.generateSession(token);
+  }
+  @Get('/load-instrument')
+  loadInstrument(): any {
+    return this.instrumentService.getInstruments();
+  }
+}
