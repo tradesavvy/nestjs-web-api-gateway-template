db417635972e33e2ddce53531715d654f0de79af
baskarmogn <baskar@antfintech.in>
2023-07-11 17:46:30 +0200

    user cleanup

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,49 +5,43 @@ import {
   Logger,
   Param,
   Patch,
-  UnauthorizedException,
   UseGuards,
 } from '@nestjs/common';
-import { EventEmitter2 } from '@nestjs/event-emitter';
+
 import { ApiTags } from '@nestjs/swagger';
 import { Observable } from 'rxjs';
 import { UserService } from './user.service';
-import { UserRiskProfileService } from './userriskprofile.service';
-import { UserbrokersService } from './userbrokers.service';
 
-import { AuthGuard } from '@nestjs/passport';
+import { AbstractJwtController } from './abstract.jwt.controller';
 
 @Controller('users')
 @ApiTags('Users')
-export class UserController {
+export class UserController extends AbstractJwtController {
+  getLogger(): Logger {
+    return this.logger;
+  }
   private readonly logger = new Logger(UserController.name);
 
-  constructor(
-    private readonly userRiskProfileService: UserRiskProfileService,
-    private readonly userbrokersService: UserbrokersService,
-    private readonly userService: UserService,
-    private readonly emitter: EventEmitter2,
-  ) {}
-
+  constructor(private readonly userService: UserService) {
+    super();
+  }
+  @UseGuards()
   @Get('ping')
   ping(): Observable<number> {
     this.logger.log('ping...');
     return this.userService.ping();
   }
 
-  @UseGuards(AuthGuard('jwt'))
   @Get('mobile/:phoneNumber')
   getUserByMobileNumber(@Param('phoneNumber') phoneNumber: string): any {
     return this.userService.getUserByMobileNumber(phoneNumber);
   }
 
-  @UseGuards(AuthGuard('jwt'))
   @Get(':username')
   getUserByUsername(@Param('username') username: string): any {
     return this.userService.getUserByUsername(username);
   }
 
-  // @UseGuards(AuthGuard('jwt'))
   @Patch(':username')
   updateUser(
     @Param('username') username: string,
@@ -60,14 +54,5 @@ export class UserController {
   @Get('email/:email')
   getUserByEmail(@Param('email') email: string): any {
     return this.userService.getUserByEmail(email);
-  }
-
-  private authorizationCheck(req: any, username: string) {
-    this.logger.log('Authenticate User: ' + JSON.stringify(req.user));
-    if (username !== req?.user?.username) {
-      this.logger.log('User in Req: ' + req?.user?.username);
-      this.logger.log('User in path: ' + username);
-      throw new UnauthorizedException();
-    }
   }
 }
