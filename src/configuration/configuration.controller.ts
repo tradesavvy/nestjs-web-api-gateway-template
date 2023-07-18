622487967ffcbe47e6dfda07cb8a922c9addefbb
baskarmogn <baskar@antfintech.in>
2023-07-18 11:09:18 +0200

    configuation added

--- src/configuration/configuration.controller.ts
+++ src/configuration/configuration.controller.ts
@@ -1,18 +1,120 @@
-import { Controller, Get, Logger } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Logger,
+  Param,
+  Post,
+  Put,
+  Req,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { ConfigurationService } from './configuration.service';
+import { AbstractJwtController } from 'src/user/abstract.jwt.controller';
+import {
+  CreateUserBrokerDto,
+  UpdateUserBrokerDto,
+  UserBroker,
+  ConnectUserBroker,
+  UpdateSortOrderDto,
+  DeleteUserBrokerDto,
+} from 'src/common/dtos/userbrokers.dto';
 
-@Controller('configuration')
+@Controller('configuration/broker')
 @ApiTags('Configuration')
-export class ConfigurationController {
+export class ConfigurationController extends AbstractJwtController {
+  getLogger(): Logger {
+    return this.logger;
+  }
   private readonly logger = new Logger(ConfigurationController.name);
 
-  constructor(private readonly configurationService: ConfigurationService) {}
-
-  @Get('ping')
-  ping(): any {
-    this.logger.log('ping...');
-    return this.configurationService.ping();
+  constructor(private readonly configurationService: ConfigurationService) {
+    super();
   }
 
+  @Get()
+  getuserBrokers(@Req() req: any): any {
+    return this.configurationService.getuserBrokers({
+      userName: req.user.username,
+    });
+  }
+
+  @Post()
+  createUserBroker(@Req() req: any, @Body() dto: CreateUserBrokerDto): any {
+    this.logger.log('Received request for createUserBroker: ' + dto);
+    dto.userName = req.user.username;
+    return this.configurationService.createUserBroker(dto);
+  }
+
+  @Put('/:id')
+  updateUserBroker(
+    @Req() req: any,
+    @Body() dto: UpdateUserBrokerDto,
+    @Param('id') id: string,
+  ): any {
+    dto.userName = req.user.username;
+    dto.userBrokerId = id;
+    this.logger.log('Received request for userBrokersService: ' + dto);
+    return this.configurationService.updateUserBroker(dto);
+  }
+
+  @Put('assign/:id')
+  assignPrimaryBroker(
+    @Req() req: any,
+    @Body() dto: any,
+    @Param('id') id: string,
+  ): any {
+    this.logger.log('Received request for assignPrimaryBroker: ' + dto);
+    return this.configurationService.assignPrimaryBroker({
+      userName: req.user.username,
+      userBrokerId: id,
+    });
+  }
+
+  @Post('disconnect')
+  disconnectUserBroker(@Req() req: any, @Body() dto: UserBroker): any {
+    this.logger.log(
+      'Received request for disconnectUserBroker: ' + JSON.stringify(dto),
+    );
+
+    dto.userName = req.user.username;
+    return this.configurationService.disconnectUserBroker(dto);
+  }
+
+  @Post('connect')
+  connectUserBroker(@Req() req: any, @Body() dto: ConnectUserBroker): any {
+    this.logger.log('Received request for connectUserBroker: ' + dto);
+
+    dto.userName = req.user.username;
+    return this.configurationService.connectUserBroker(dto);
+  }
+
+  @Post('activate')
+  enableTrade(@Req() req: any, @Body() dto: UserBroker): any {
+    dto.userName = req.user.username;
+    this.logger.log('Received request for enableTrade: ' + dto);
+    return this.configurationService.enableTrade(dto);
+  }
+
+  @Post('sort')
+  updateSortOrder(@Req() req: any, @Body() dto: UpdateSortOrderDto): any {
+    dto.userName = req.user.username;
+    this.logger.log('Received request for updateSortOrder: ' + dto);
+    return this.configurationService.updateSortOrder(dto);
+  }
+
+  @Delete(':id')
+  deleteUserBroker(
+    @Req() req: any,
+
+    @Param('id') id: string,
+  ): any {
+    const dto: DeleteUserBrokerDto = {
+      userName: req.user.username,
+      userBrokerId: id,
+    };
+    this.logger.log('Received request for deleteUserBroker: ' + dto);
+    return this.configurationService.deleteUserBroker(dto);
+  }
 }
--- src/configuration/configuration.service.ts
+++ src/configuration/configuration.service.ts
@@ -1,8 +1,10 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class ConfigurationService {
+  private readonly logger = new Logger(ConfigurationService.name);
+
   constructor(
     @Inject('CONFIGURATION') private readonly configurationClient: ClientProxy,
   ) {}
@@ -10,5 +12,60 @@ export class ConfigurationService {
   ping(): any {
     const pattern = { cmd: 'ping' };
     return this.configurationClient.send<any>(pattern, {});
+  }
+
+  async createUserBroker(dto: any): Promise<any> {
+    this.logger.log(
+      'Received request for createUserBroker: ' + JSON.stringify(dto),
+    );
+    const pattern = { cmd: 'createUserBroker' };
+    return this.configurationClient.send<any>(pattern, dto);
+  }
+  getuserBrokers(payload: any): any {
+    const pattern = { cmd: 'getuserBrokers' };
+    this.logger.log('getuserBrokers' + payload);
+    return this.configurationClient.send<any>(pattern, payload);
+  }
+  async assignPrimaryBroker(dto: any): Promise<any> {
+    this.logger.log('Received request for assignPrimaryBroker: ' + dto);
+    const pattern = { cmd: 'assignPrimaryBroker' };
+    return this.configurationClient.send<any>(pattern, dto);
+  }
+  async updateUserBroker(dto: any): Promise<any> {
+    this.logger.log('Received request for userBrokersService: ' + dto);
+    const pattern = { cmd: 'updateUserBroker' };
+    return this.configurationClient.send<any>(pattern, dto);
+  }
+
+  async disconnectUserBroker(dto: any): Promise<any> {
+    this.logger.log('Received request for userBrokersService: ' + dto);
+    const pattern = { cmd: 'disconnectUserBroker' };
+    return this.configurationClient.send<any>(pattern, dto);
+  }
+
+  async connectUserBroker(dto: any): Promise<any> {
+    this.logger.log(
+      'Received request for connectUserBroker: ' + JSON.stringify(dto),
+    );
+    const pattern = { cmd: 'connectUserBroker' };
+    return this.configurationClient.send<any>(pattern, dto);
+  }
+
+  async enableTrade(dto: any): Promise<any> {
+    this.logger.log('Received request for enableTrade: ' + dto);
+    const pattern = { cmd: 'enableTrade' };
+    return this.configurationClient.send<any>(pattern, dto);
+  }
+
+  async updateSortOrder(dto: any): Promise<any> {
+    this.logger.log('Received request for updateSortOrder: ' + dto);
+    const pattern = { cmd: 'updateSortOrder' };
+    return this.configurationClient.send<any>(pattern, dto);
+  }
+
+  async deleteUserBroker(dto: any): Promise<any> {
+    this.logger.log('Received request for deleteUserBroker: ' + dto);
+    const pattern = { cmd: 'deleteuserBroker' };
+    return this.configurationClient.send<any>(pattern, dto);
   }
 }
